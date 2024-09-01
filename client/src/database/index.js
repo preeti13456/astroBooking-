7986964a62daf66a4555b4e4da1b98e7525f20b2
Preeti Sharma <36129347+preeti13456@users.noreply.github.com>
2024-09-01 21:53:29 +0530

    Add files via upload

--- client/src/database/index.js
+++ client/src/database/index.js
@@ -1,1 +1,9 @@
+const Sequelize = require('sequelize');
+const config = require('./config');
 
+const sequelize = new Sequelize(config.database, {
+  dialect: 'mysql',
+  logging: false,
+});
+
+module.exports = sequelize;
